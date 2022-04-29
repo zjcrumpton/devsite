ace7176aa09e88d5c41b398ba3a019cb3bcca995
zjcrumpton <zjcrumpton@gmail.com>
2022-04-28 22:01:09 -0600

    implement logo

--- src/components/TopNav.svelte
+++ src/components/TopNav.svelte
@@ -1,9 +1,14 @@
 <script lang="ts">
   import NavLink from "@components/NavLink.svelte";
+  import Hamsa from "@components/Hamsa.svelte";
 </script>
 
-<div class="flex justify-between py-10 px-20 text-white">
-  <h1 class="font-extrabold text-white">Zac Crumpton</h1>
+<div class="flex justify-between py-10 px-20 text-white text-lg">
+  <div class="font-extrabold text-white text-2xl cursor-pointer">
+    <a href="/" class="flex gap-1.5 items-center">
+      <span class="mr-2"><Hamsa/></span><span>Zac</span><span>Crumpton</span>
+    </a>
+  </div>
   <div class="flex gap-10">
     <NavLink path="/" name="Home" />
     <NavLink path="/projects" name="Projects" />
